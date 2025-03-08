8b24ef51653ed65c58a61cf8530cb9f610bb4cc8
Nitesh Giri <nitesh.giri2@gmail.com>
2025-03-08 16:33:53 +0100

    Add project files.

---
+++ SumTwoNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Declare variables for the numbers
+int num1, num2, sum;
+
+// user input
+Console.WriteLine("Enter the first number:");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number:");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+// Calculate the sum
+sum = num1 + num2;
+
+// result of sum
+Console.WriteLine($"Sum of {num1} and {num2} is = {sum}");
